5047935f15b94ba4ca5ad0020a521152d7075b0d
Taras <Taras.Fomin@gmail.com>
2025-07-21 23:19:29 +0300

    feat: external spotify and youtubei extractors (#1)

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,16 +1,49 @@
-import { Listener } from '@sapphire/framework';
-import { DefaultExtractors } from '@discord-player/extractor';
+import {
+  AppleMusicExtractor,
+  AttachmentExtractor,
+  ReverbnationExtractor,
+  SoundCloudExtractor,
+  VimeoExtractor,
+} from "@discord-player/extractor";
+import { Listener } from "@sapphire/framework";
+import { SpotifyExtractor } from "discord-player-spotify";
+import { YoutubeiExtractor } from "discord-player-youtubei";
 
 export class UserEvent extends Listener {
-	public constructor(context: Listener.LoaderContext, options: Listener.Options) {
-		super(context, {
-			...options,
-			once: true
-		});
-	}
+  public constructor(
+    context: Listener.LoaderContext,
+    options: Listener.Options
+  ) {
+    super(context, {
+      ...options,
+      once: true,
+    });
+  }
 
-	public async run() {
-		await this.container.client.player.extractors.loadMulti(DefaultExtractors);
-		return this.container.client.logger.info(`Successfully logged in as: ${this.container.client.user?.username}`);
-	}
+  public async run() {
+    await this.container.client.player.extractors.loadMulti([
+      SoundCloudExtractor,
+      AttachmentExtractor,
+      ReverbnationExtractor,
+      AppleMusicExtractor,
+      YoutubeiExtractor,
+      VimeoExtractor,
+    ]);
+    await this.container.client.player.extractors.register(SpotifyExtractor, {
+      clientId: process.env.SPOTIFY_CLIENT_ID,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+      market: process.env.SPOTIFY_MARKET,
+      ...(process.env.SPOTIFY_MIRROR_URL && {
+        createStream: (ext, url) => {
+          const uri = url.replace(/^.*(track)\/([\w\d]+).*$/, "spotify:$1:$2");
+          return Promise.resolve(
+            `${process.env.SPOTIFY_MIRROR_URL}${encodeURIComponent(uri)}`
+          );
+        },
+      }),
+    });
+    return this.container.client.logger.info(
+      `Successfully logged in as: ${this.container.client.user?.username}`
+    );
+  }
 }
